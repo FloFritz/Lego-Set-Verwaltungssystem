1ff62e9f77ee92e6c3bc46c9722ff46d66434d07
FloFritz <florian.fritz.1@gmx.de>
2025-04-23 15:48:04 +0200

    Ordnerstruktur angepasst

---
+++ Planung/Pflichtenlastenheft/GUI prototyp/GUI prototyp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace LegoSetManagement
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InventarButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logik für den Wechsel zum Inventar-Tab
+            MainTabControl.SelectedIndex = 1; // Tab "Inventar" aktivieren
+        }
+
+        private void AddSetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logik für den Wechsel zum "Set Hinzufügen"-Tab
+            MainTabControl.SelectedIndex = 2; // Tab "Set Hinzufügen" aktivieren
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logik für den Wechsel zum Export-Tab
+            MainTabControl.SelectedIndex = 3; // Tab "Export" aktivieren
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Popup-Dialog für Login/Registrierung anzeigen
+            var result = MessageBox.Show("Möchten Sie sich anmelden oder registrieren?", "Login/Registrierung", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // Logik für Anmeldung
+                MessageBox.Show("Anmeldung erfolgreich!", "Login", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (result == MessageBoxResult.No)
+            {
+                // Logik für Registrierung
+                MessageBox.Show("Registrierung erfolgreich!", "Registrieren", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
