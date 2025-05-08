43c6e25c9a97d0030557372beb6142b3554cbeea
FloFritz <florian.fritz.1@gmx.de>
2025-05-08 20:16:30 +0200

    HomePage hinzugefügt

--- Lego-Set-Verwaltungssystem_Code/Lego-Set-Verwaltungssytem/Lego-Set-Verwaltungssytem/Views/HomePage.xaml.cs
+++ Lego-Set-Verwaltungssystem_Code/Lego-Set-Verwaltungssytem/Lego-Set-Verwaltungssytem/Views/HomePage.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Controls;
+using Lego_Set_Verwaltungssytem.Data;
+using Lego_Set_Verwaltungssytem.Services;
 
 namespace Lego_Set_Verwaltungssytem.Views
 {
@@ -21,6 +23,34 @@ namespace Lego_Set_Verwaltungssytem.Views
         public HomePage()
         {
             InitializeComponent();
+            LadeInfos();
         }
+
+        private void LadeInfos()
+        {
+            string name = App.Current.Properties["BenutzerName"]?.ToString() ?? "Gast";
+            txtBegruessung.Text = $"Hallo {name}!";
+
+            if (name != "Gast")
+            {
+                using var db = new AppDbContext();
+                int anzahl = db.BenutzerSets.Count(bs => bs.Benutzer.Benutzername == name);
+                txtSammlungsInfo.Text = $"Du hast aktuell {anzahl} Sets in deiner Sammlung.";
+            }
+
+            txtLizenzInfo.Text = string.IsNullOrWhiteSpace(RebrickableService.ApiKey)
+                ? "⚠ Kein API-Key gefunden – Suche und Import gesperrt."
+                : "✔ API-Key ist aktiv – alle Funktionen verfügbar.";
+        }
+
+        private void BtnSammlung_Click(object sender, RoutedEventArgs e) =>
+            NavigationService.Navigate(new SammlungPage());
+
+        private void BtnSuche_Click(object sender, RoutedEventArgs e) =>
+            NavigationService.Navigate(new SetSuchePage());
+
+        private void BtnStatistik_Click(object sender, RoutedEventArgs e) =>
+            NavigationService.Navigate(new StatistikenPage());
     }
+
 }
